65b7b24e44bfc999a1b1834a229cac7b44246397
Dyvrais <78991597+Dyvrais@users.noreply.github.com>
2021-06-30 14:52:58 -0400

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-$b = 3;
-$c = 4;
-$a = sqrt(($b ** 2) + ($c ** 2));
-echo "Valor del lado b = $b", "<br />";
-echo "Valor del lado c = $c", "<br />";
-echo "El valor de la hipotenusa del triangulo rectangulo es de $a";
-?>
