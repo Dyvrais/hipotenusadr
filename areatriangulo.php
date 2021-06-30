d3927f86addf1f21b764caea9fc27c20fd559b98
Dyvrais <78991597+Dyvrais@users.noreply.github.com>
2021-06-30 14:39:50 -0400

    Add files via upload

---
+++ areatriangulo.php
@@ -0,0 +1,8 @@
+<?php
+$b = 3;
+$c = 4;
+$a = sqrt(($b ** 2) + ($c ** 2));
+echo "Valor del lado b = $b", "<br />";
+echo "Valor del lado c = $c", "<br />";
+echo "El valor de la hipotenusa del triangulo rectangulo es de $a";
+?>
